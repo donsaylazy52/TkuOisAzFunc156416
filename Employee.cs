cc97279648882f3570d6669701ba7ae15c0de9b1
RayRay <156416@o365.tku.edu.tw>
2020-07-22 15:22:45 +0800

    First Commit.

---
+++ Employee.cs
@@ -0,0 +1,8 @@
+namespace TkuOisAzFunc156416 {
+    public class Employee {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string EmpName { get; set; }
+        public string EmpEmail { get; set; }
+    }
+}
